8c733b8b7d5e94651ef8d97cd2bb8466fe2e6db5
Dunnia <dunnia.alvarez@gmail.com>
2018-08-06 18:27:13 +0200

    edita el password

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const bcryptSalt = 10;
+const bcrypt = require("bcrypt");
 
 // All Users
 router.get('/', (req, res, next) => {
@@ -14,18 +16,58 @@ router.get('/:id', (req, res, next) => {
 
 // Edit User
 router.put('/edit/:id', (req, res, next) => {
-	
 	User.findById(req.params.id).then(user => {
-		const { username, name, lastname, email, paswword, category } = req.body;
-	
-	const updates = { username, name, lastname, email, paswword, category };
-	
-	User.findByIdAndUpdate(req.params.id, updates, { new: true })
-		.then((object) => res.json(object))
-		.catch((e) => next(e));
-})
-});
 
+		const { username, name, lastname, email, oldpassword, newpassword, category } = req.body;
+
+		const changeFields = new Promise((resolve, reject) => {
+			if (!username || !email || !name || !lastname || !category) {
+				reject(new Error("Username ,name ,lastname and email are required."));
+			} else {
+				resolve();
+			}
+		});
+
+		const changePassword = new Promise((resolve, reject) => {
+			if ((oldpassword !== "" && newpassword === "") || (oldpassword === "" && newpaswword !== "")) {
+				reject(new Error("You should fill both password fields"));
+			} else if (oldpassword && newpassword && !bcrypt.compareSync(oldpassword, req.user.paswword)) {
+				reject(new Error("Password incorrect"));
+			} else {
+				resolve();
+			}
+		});
+
+		changeFields.then(() => {
+			return changePassword;
+		})
+			.then(() => {
+				return User.findOne({ username, _id: { $ne: req.params.id } });
+			})
+			.then(user => { // Change password
+				if (oldpassword && newpassword) {
+					const salt = bcrypt.genSaltSync(bcryptSalt);
+					const hashPass = bcrypt.hashSync(newpassword, salt);
+					return User.findByIdAndUpdate(req.params.id, { password: hashPass });
+				}
+			})
+			.then(user => {
+				if (user) {
+					throw new Error('ohh username exists');
+				}
+				return User.findByIdAndUpdate(req.params.id, {
+					username,
+					name,
+					lastname,
+					category,
+					email
+				}, {new:true});
+			})
+			.then(user => res.json(user))
+			.catch(err => next(err));
+	});
+});
+	
 // Delete user
 router.delete('/delete/:id', (req, res, next) => {
 	User.findByIdAndRemove(req.params.id)
