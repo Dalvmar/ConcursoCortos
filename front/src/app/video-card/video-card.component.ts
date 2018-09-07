21e1ad86eafa07defbbf53a14c01d42cdb815a24
Dunnia <dunnia.alvarez@gmail.com>
2018-09-07 11:56:46 +0200

    alertas like y unlike

--- front/src/app/video-card/video-card.component.ts
+++ front/src/app/video-card/video-card.component.ts
@@ -1,148 +1,4 @@
 
-// import { Component, OnInit,Input } from '@angular/core';
-// import { VideoService } from '../../services/video.service';
-// import { CommentsService } from '../../services/comments.service';
-// import { SessionService } from '../../services/session.service';
-
-
-
-// @Component({
-//   selector: 'app-video-card',
-//   templateUrl: './video-card.component.html',
-//   styleUrls: ['./video-card.component.css']
-// })
-// export class VideoCardComponent implements OnInit {
-  
-//   videoList: Array<any>;
-// 	@Input()video;
-// 	comments;
-
-// 	countComments:number=0;
-// 	user;
-// 	search:String;
-// 	comment;
-// 	commentvideo: string = "";
-// 	hidden:boolean=false;
-// 	status;
-// 	// contLike:number=1;
-// 	@Input() likes =0;
-// 	@Input() unlikes =0;
-// 	since:number=0;
-// 	totalVideos: number = 0;
-
-
-  
-//   constructor(
-//     private videoService: VideoService,
-// 		private commentsService: CommentsService,
-// 		private sessionService: SessionService
-//   ) { }
-
-//   ngOnInit() {
-//     this.getlistVideo();
-	
-
-// 		this.sessionService.isLogged().subscribe(user=>{
-// 			this.user=user
-// 			// console.log(this.user.status)
-// 			if(this.user.status===500){
-// 				this.hidden=false
-		
-// 			}else
-// 			this.hidden=true;
-// 			;
-		
-// 		})
-// 		this.hidden=false;
-//   }
-
-//   getlistVideo() {
-		
-// 		this.videoService.getlistVideos().subscribe((data) => {
-		
-// 			data.forEach((obj) => {
-// 				if (obj.video.includes('instagram')) {
-// 					obj.video = obj.video + "embed";
-					
-// 				} else if (obj.video.includes('youtube')) {
-// 					obj.video = obj.video.replace('watch?v=', 'embed/');
-// 				} else if (obj.video.includes('vimeo')) {
-// 					obj.video = obj.video.replace('https://vimeo.com', 'https://player.vimeo.com/video')
-// 				}
-
-// 				console.log(obj)
-// 			});
-// 			this.videoList = data;
-		
-// 			console.log(this.videoList)
-// 		});
-// 	}
-
-
-// 	cambiarDesde( valor: number ) {
-
-// 		let desde = this.since + valor;
-	
-// 		if ( desde >= this.totalVideos ) {
-// 		  return;
-// 		}
-	
-// 		if ( desde < 0 ) {
-// 		  return;
-// 		}
-	
-// 		this.since += valor;
-// 		this.getlistVideo();
-	
-// 	  }
-	
-// 	toggleHidden(e,i) {
-// 		[].slice.call(e.currentTarget.closest('.info').childNodes).forEach(e=>{
-// 			if(e.classList.contains('comment-input')) e.classList.toggle('active')
-// 		})
-// 	}
-
-
-// 	saveComment(videoId, comment, i) {
-// 		this.commentsService.saveComment(videoId, comment, this.user._id)
-// 		.subscribe(video => {
-// 			console.log(video)
-		
-// 			this.videoList[i].commment = video.commment;
-
-
-// 		})
-// 	}
-
-	
-// 	 like (videoId) { 
-// 		console.log(videoId)
-		
-// 		this.videoService.saveLikes( videoId)
-// 		.subscribe(video=>{
-// 			console.log(video.like);
-// 			 this.likes=video.like;
-// 			 swal('LIKE', ':)', 'success');
-             
-// 		})
-		
-// 		}
-
-			
-// 	unlike (videoId) { 
-// 		console.log(videoId)
-		
-// 		this.videoService.saveUnLikes(videoId)
-// 		.subscribe(video=>{
-// 			console.log(video);
-// 			 this.unlikes=video.unlike;
-// 			 swal('UNLIKE', ':(', 'error');
-// 		})
-	
-// 		}
-
-// }
-
 import { Component, OnInit,Input } from '@angular/core';
 import { VideoService } from '../../services/video.service';
 import { CommentsService } from '../../services/comments.service';
@@ -167,7 +23,7 @@ export class VideoCardComponent implements OnInit {
 
 	hidden:boolean=false;
 	status;
-	// contLike:number=1;
+	
 	@Input() likes =0;
 	@Input() unlikes =0;
   
@@ -240,6 +96,7 @@ export class VideoCardComponent implements OnInit {
 		.subscribe(video=>{
 			console.log(video.like);
 			 this.likes=video.like;
+			 swal('LIKE', ':)', 'success');
 			
 			
 		})
@@ -254,8 +111,26 @@ export class VideoCardComponent implements OnInit {
 		.subscribe(video=>{
 			console.log(video);
 			 this.unlikes=video.unlike;
+			 swal('UNLIKE', ':(', 'error');
 		})
 	
 		}
+
+// 	cambiarDesde( valor: number ) {
+
+// 		let desde = this.since + valor;
+	
+// 		if ( desde >= this.totalVideos ) {
+// 		  return;
+// 		}
+	
+// 		if ( desde < 0 ) {
+// 		  return;
+// 		}
+	
+// 		this.since += valor;
+// 		this.getlistVideo();
+	
+// 	  }
 
 }
